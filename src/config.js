a73b5e15155e5a6cfeaae3878f077771606f25dc
Yesid Toloza <yesidtoloza2005@gmail.com>
2026-05-04 21:58:10 -0500

    fix: update iptables rule priority to prevent Tailscale/Docker conflicts

--- src/config.js
+++ src/config.js
@@ -24,13 +24,13 @@ module.exports.WG_ALLOWED_IPS = process.env.WG_ALLOWED_IPS || '0.0.0.0/0, ::/0';
 
 module.exports.WG_PRE_UP = process.env.WG_PRE_UP || '';
 module.exports.WG_POST_UP = process.env.WG_POST_UP || `
-iptables -t nat -A POSTROUTING -s ${module.exports.WG_DEFAULT_ADDRESS.replace('x', '0')}/24 -o ${module.exports.WG_DEVICE} -j MASQUERADE;
-ip6tables -t nat -A POSTROUTING -s ${module.exports.WG_DEFAULT_ADDRESS_V6.replace('x', '0')}/64 -o ${module.exports.WG_DEVICE} -j MASQUERADE;
-iptables -A INPUT -p udp -m udp --dport ${module.exports.WG_PORT} -j ACCEPT;
-iptables -A FORWARD -i wg0 -j ACCEPT;
-iptables -A FORWARD -o wg0 -j ACCEPT;
-ip6tables -A FORWARD -i wg0 -j ACCEPT;
-ip6tables -A FORWARD -o wg0 -j ACCEPT;
+iptables -t nat -I POSTROUTING 1 -s ${module.exports.WG_DEFAULT_ADDRESS.replace('x', '0')}/24 -o ${module.exports.WG_DEVICE} -j MASQUERADE;
+ip6tables -t nat -I POSTROUTING 1 -s ${module.exports.WG_DEFAULT_ADDRESS_V6.replace('x', '0')}/64 -o ${module.exports.WG_DEVICE} -j MASQUERADE;
+iptables -I INPUT 1 -p udp -m udp --dport ${module.exports.WG_PORT} -j ACCEPT;
+iptables -I FORWARD 1 -i wg0 -j ACCEPT;
+iptables -I FORWARD 1 -o wg0 -j ACCEPT;
+ip6tables -I FORWARD 1 -i wg0 -j ACCEPT;
+ip6tables -I FORWARD 1 -o wg0 -j ACCEPT;
 `.split('\n').join(' ');
 
 module.exports.WG_PRE_DOWN = process.env.WG_PRE_DOWN || '';
